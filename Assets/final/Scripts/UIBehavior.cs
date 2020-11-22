a2e8541844d737939b4a92deb737952e0f2a6d98
Chelsea <cl773@cornell.edu>
2020-11-21 16:28:54 -0800

    sound effect

--- Assets/final/Scripts/UIBehavior.cs
+++ Assets/final/Scripts/UIBehavior.cs
@@ -9,9 +9,16 @@ public class UIBehavior : MonoBehaviour
     GameObject canvas;
     [SerializeField]
     TextMeshProUGUI text;
+    [SerializeField]
+    TextMeshProUGUI titletext;
+
+    public AudioSource scoreSource;
+
+
     // Start is called before the first frame update
     void Start()
     {
+    	scoreSource = GetComponent<AudioSource> ();
     }
 
     // Update is called once per frame
@@ -30,16 +37,22 @@ public class UIBehavior : MonoBehaviour
 
         if ((name == "RecycleCollider") &&(other.gameObject.CompareTag("Recycle"))){
             canvas.SetActive(true);
-            text.text = "Congrats, you just made the world a cleaner place. \nEach year, more than 8 million tons of plastic is dumped into our oceans!";
+            scoreSource.Play();
+            titletext.text = "<color=blue>Recycle</color>";
+            text.text = "Each year, more than 8 million tons of plastic is dumped into our oceans!";
         }
         else if ((name == "BiohazardCollider") && (other.gameObject.CompareTag("Biohazard")))
         {
             canvas.SetActive(true);
-            text.text = "Congrats, you just made the world a cleaner place. \nEach year, over 180 million tons of Toxic Waste is dumped into our Oceans, Rivers, and Lakes!";
+            scoreSource.Play();
+            titletext.text = "<color=red>Hazardous Waste</color>";
+            text.text = "Each year, over 180 million tons of Toxic Waste is dumped into our Oceans, Rivers, and Lakes!";
         }else if ((name == "WasteCollider") && (other.gameObject.CompareTag("Waste")))
         {
             canvas.SetActive(true);
-            text.text = "Congrats, you just made the world a cleaner place. \nEach year, about 100 million marine animals die from human waste dumped in the ocean";
+            scoreSource.Play();
+            titletext.text = "<color=green>Waste</color>";
+            text.text = "Each year, about 100 million marine animals die from human waste dumped in the ocean";
         }
     }
 
